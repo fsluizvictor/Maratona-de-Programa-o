3a1264f3f61b62f67e80b6e3c4b08eafac213dcb
fsluizvictor <fs.luizvictor@gmail.com>
2021-02-22 20:12:35 -0300

    implementando o algoritmo de Chamos e Hoey

--- geometria.java
+++ geometria.java
@@ -3,50 +3,80 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Scanner;
 
-class Ponto implements Comparable<Ponto> {
-    int esquerdo;
-    int direito;
-    int dist;
+class Geometria {
+    static class Ponto implements Comparable<Ponto> {
+        static int id;
+        int esquerdo;
+        int direito;
+        int dist;
 
-    public Ponto() {
-    }
-
-    public Ponto(int esquerdo, int direito) {
-        this.esquerdo = esquerdo;
-        this.direito = direito;
-    }
-
-    public int getEsquerdo() {
-        return this.esquerdo;
-    }
-
-    public int getDireito() {
-        return this.direito;
-    }
-
-    public void setDist(int d) {
-        this.dist = d;
-    }
-
-    @Override
-    public int compareTo(Ponto p) {
-        if (this.getDireito() < p.getDireito()) {
-            return -1;
-        } else if (this.getDireito() > p.getDireito()) {
-            return 1;
+        public Ponto() {
         }
-        return 0;
+
+        public Ponto(int esquerdo, int direito) {
+            id++;
+            this.esquerdo = esquerdo;
+            this.direito = direito;
+        }
+
+        public int getEsquerdo() {
+            return this.esquerdo;
+        }
+
+        public int getDireito() {
+            return this.direito;
+        }
+
+        public void setDist(int d) {
+            this.dist = d;
+        }
+
+        @Override
+        public int compareTo(Ponto p) {
+            if (this.getDireito() < p.getDireito()) {
+                return -1;
+            } else if (this.getDireito() > p.getDireito()) {
+                return 1;
+            }
+            return 0;
+        }
     }
 
-    static int calculaDist(Ponto p1, Ponto p2) {
+    static class Esquerdo extends Ponto implements Comparable<Ponto> {
+        @Override
+        public int compareTo(Ponto p) {
+            if (this.getEsquerdo() < p.getEsquerdo()) {
+                return -1;
+            } else if (this.getEsquerdo() > p.getEsquerdo()) {
+                return 1;
+            }
+            return 0;
+        }
+    }
+
+    static Ponto calculaDist(Ponto p1, Ponto p2) {
         int dist = (int) Math.sqrt(
                 Math.pow(p2.getDireito() - p1.getDireito(), 2) + Math.pow(p2.getEsquerdo() - p1.getEsquerdo(), 2));
         return dist;
     }
 
-
     static Ponto menorDistancia(ArrayList<Ponto> pontos, int qtd) {
         return pontos.get(0);
+    }
+
+    Ponto recursao(int l, int n, ArrayList<Ponto> pontos) {
+        if (n - l <= 3) {
+            int minDist = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    int aux = calculaDist(pontos.get(i), pontos.get(j));
+                    if (aux < minDist) {
+                        minDist = aux;
+                    }
+                }
+            }
+            return minDist;
+        }
     }
 
     public static void main(String[] args) {
@@ -64,4 +94,5 @@ class Ponto implements Comparable<Ponto> {
 
         Collections.sort(pontos);
     }
+
 }
